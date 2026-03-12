6df2aa5de35e151cdd0e48fc519690aaa1a61fb6
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2026-03-12 21:26:08 +0100

    add logo and fav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,5 @@
-import * as motion from "motion/react-client";
+import { Logo } from "@/components/shared/Logo/Logo";
 
 export default function Home() {
-  return (
-    <motion.h1
-      className="text-4xl font-bold text-green-900"
-      initial={{
-        opacity: 0,
-        y: 40,
-      }}
-      animate={{
-        opacity: 1,
-        y: 0,
-      }}
-      transition={{
-        duration: 0.8,
-        ease: [0.22, 1, 0.36, 1],
-        delay: 0.2,
-      }}
-    >
-      Nature Guardians
-    </motion.h1>
-  );
+  return <Logo className="w-48 p-1"/>; 
 }
